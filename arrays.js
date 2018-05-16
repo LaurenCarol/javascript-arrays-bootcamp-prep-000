2520195aca476ab35bb923a1af8b54f0014f47ea
laurenelizabethmail@yahoo.com <laurenelizabethmail@yahoo.com>
2018-05-16 22:05:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,9 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
   array.shift(element)
   return array
 }
+
+function removeElementFromBeginningOfArray(array, element) {
+  array.slice(element)
+  return array
+}
+
